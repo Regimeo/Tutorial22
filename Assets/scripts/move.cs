1fb4d060b46031f75d402582607ef491249cbce2
regicat <regicat@users.noreply.github.com>
2019-07-18 01:14:45 -0400

    challenge commit

---
+++ Assets/scripts/move.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public Vector2 point1;
+    public Vector2 point2;
+    public float speed;
+    // Update is called once per frame
+    void Update()
+    {
+        float time = Mathf.PingPong(Time.time * speed, 1);
+        transform.position = Vector2.Lerp(point1, point2, time);
+    }
+}
--- Assets/scripts/playercontroller.cs
+++ Assets/scripts/playercontroller.cs
@@ -1,19 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class playercontroller : MonoBehaviour
 {
     private Rigidbody2D rb2d;
+    private int score;
+    private int lives;
     public float speed;
     public float jump;
+    public Text scoreText;
+    public Text livesText;
+    public Text winText;
+    public AudioClip MusicClip;
+    public AudioClip MusicClip2;
+    public AudioSource MusicSource;
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-    }
-    void Update()
-    {
-        
+        score = 0;
+        lives = 3;
+        SetAllText();
+        winText.text = "";
+        MusicSource.clip = MusicClip;
+        MusicSource.Play();
     }
     void FixedUpdate()
     {
@@ -24,6 +33,33 @@ public class playercontroller : MonoBehaviour
         if (Input.GetKey("escape"))
         Application.Quit();
     }
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+     if (other.gameObject.CompareTag("pickup"))
+     {
+          other.gameObject.SetActive(false);
+          score = score + 1;
+          SetAllText();
+     }
+     if (score == 4)
+     {
+         transform.position = new Vector2(30.0f, -2.0f);
+         Camera.main.transform.position = new Vector3 (30.0f, 0.5f, -10.0f);
+         lives = 3;
+         SetAllText();
+     }
+     if (other.gameObject.CompareTag("enemy"))
+     {
+          other.gameObject.SetActive(false);
+          lives = lives - 1;
+          SetAllText();
+        if (lives == 0)
+        {
+         Destroy(this);
+         winText.text = "You Lose!";
+        }
+     }
+    }
     void OnCollisionStay2D(Collision2D collision)
     {
         if(collision.collider.tag == "ground")
@@ -33,5 +69,17 @@ public class playercontroller : MonoBehaviour
                 rb2d.AddForce(new Vector2(0, jump), ForceMode2D.Impulse);
             }
         }
+    }
+    void SetAllText ()
+    {
+        scoreText.text = "Score: " + score.ToString();
+        livesText.text = "Lives: " + lives.ToString();
+        if (score >= 8)
+        {
+            winText.text = ("You Win!");
+            MusicSource.clip = MusicClip2;
+            MusicSource.Play();
+        }
+
     }
 }
